2a57bae852b37ca3157a3efd5c38d1a016170fe5
CAUA HENRIQUE SOUZA COSTA <caua.3932@aluno.pr.senac.br>
2023-09-06 14:57:28 -0300

    add arquivo

---
+++ src/main/java/com/mycompany/exercicio1/teste.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exercicio1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author caua.3932
+ */
+public class teste {
+    public static void main (String [] args ){
+        
+       Scanner scanner = new Scanner(System.in); 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
